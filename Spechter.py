05002726f7ca11315ea17eb0ad926b32f60a62ba
Gencisama <39381217+Gencisama@users.noreply.github.com>
2022-10-18 20:23:51 +0200

    Update Spechter.py

--- Spechter.py
+++ Spechter.py
@@ -4,4 +4,4 @@ st.selectbox('Pick one', ['cats', 'dogs'])
 st.selectbox('Pick secret', st.secrets["secret_list"])
 
 if st.button("ADD"):
-  st.secrets["secret_list"].append("New Item)
+  st.secrets["secret_list"].append("New Item")
