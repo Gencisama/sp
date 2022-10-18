aa093576622e0b161758380a08915dc04fba4832
Gencisama <39381217+Gencisama@users.noreply.github.com>
2022-10-18 20:35:39 +0200

    Delete Spechter.py

--- Spechter.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.selectbox('Pick one', ['cats', 'dogs'])
-st.selectbox('Pick secret', st.secrets["secret_list"])
-
-if st.button("ADD"):
-  st.secrets["secret_list"].append("New Item")
